3f71073b0bd69d9c1de2d5b116b5c10aa1109b59
LT <rav3tey@gmail.com>
2019-11-05 16:22:07 +0800

    Updates

---
+++ precache-manifest.4bdbe2544cbdcb3da4a73d4a57005686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca04584792ec30dcf4ed2a4578b30d5e",
+    "url": "/for-tobias/index.html"
+  },
+  {
+    "revision": "eecc9fe52eed9074d6a3",
+    "url": "/for-tobias/static/css/2.55c66c37.chunk.css"
+  },
+  {
+    "revision": "84af187d23ad58937f70",
+    "url": "/for-tobias/static/css/main.4dd0ffdb.chunk.css"
+  },
+  {
+    "revision": "eecc9fe52eed9074d6a3",
+    "url": "/for-tobias/static/js/2.80f09baa.chunk.js"
+  },
+  {
+    "revision": "84af187d23ad58937f70",
+    "url": "/for-tobias/static/js/main.9b4a3df2.chunk.js"
+  },
+  {
+    "revision": "ac8147143d2216821c09",
+    "url": "/for-tobias/static/js/runtime-main.8af2490c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/for-tobias/precache-manifest.e5fed5c376576c06b21476373a41d5e2.js"
+  "/for-tobias/precache-manifest.4bdbe2544cbdcb3da4a73d4a57005686.js"
 );
 
 self.addEventListener('message', (event) => {
